64262f9b102f68334d00d6eaafba57e052b480e4
allagui-mar <>
2023-11-28 14:50:15 +0100

    fr

---
+++ src/pages/About.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='about'>
+        <h2>About Us</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Expedita nisi velit quos! Similique, ex aut ratione maiores
+             laboriosam odit dolores quisquam beatae.
+            Ullam consequatur id tempore ducimus odit.
+             Blanditiis, error.
+         </p>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Expedita nisi velit quos! Similique, ex aut ratione maiores
+             laboriosam odit dolores quisquam beatae.
+            Ullam consequatur id tempore ducimus odit.
+             Blanditiis, error.
+         </p>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Expedita nisi velit quos! Similique, ex aut ratione maiores
+             laboriosam odit dolores quisquam beatae.
+            Ullam consequatur id tempore ducimus odit.
+             Blanditiis, error.
+         </p>
+      
+    </div>
+  )
+}
+
+export default About
---
+++ src/pages/Home.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='home'>
+        <h2>Welcome</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Expedita nisi velit quos! Similique, ex aut ratione maiores
+             laboriosam odit dolores quisquam beatae.
+            Ullam consequatur id tempore ducimus odit.
+             Blanditiis, error.
+         </p>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Expedita nisi velit quos! Similique, ex aut ratione maiores
+             laboriosam odit dolores quisquam beatae.
+            Ullam consequatur id tempore ducimus odit.
+             Blanditiis, error.
+         </p>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Expedita nisi velit quos! Similique, ex aut ratione maiores
+             laboriosam odit dolores quisquam beatae.
+            Ullam consequatur id tempore ducimus odit.
+             Blanditiis, error.
+         </p>
+      
+    </div>
+  )
+}
+
+export default Home
